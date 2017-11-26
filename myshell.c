3f868eef719a8e29952cfc2cf55d62940a7a2796
Galsad <galsdh@gmail.com>
2017-11-26 14:55:20 -0800

    signal

--- myshell.c
+++ myshell.c
@@ -1,13 +1,23 @@
-//
-// Created by gal on 11/23/17.
-//
-
 #include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
 #include <unistd.h>
 #include <sys/wait.h>
+#include <signal.h>
+
+
+// TODO - errors should be written to strerr, need to write exit(1)
+// TODO - CTRL-D exits?
+// TODO - free struct memory (finalize)
+// TODO - move sig_action to prepare
+// TODO - make sure 1 is always returned from process arglist
+// TODO - check success of execvp, dup (and to execute exit(1)if needed)
+// TODO - take care of zombies (SIGCHILD)
+// TODO - prepare returns 0 on success
+// TODO - skeleton calls finilize and returns 0 on success
+// TODO - separate the waits in the while loops
+
 
 // locates the pipe index if exists
 int locate_pipe(int count, char ** arglist){
@@ -35,59 +45,24 @@ void take_care_of_single_child(char** arglist){
     }
 }
 
-void take_care_of_two_children(int pipe_index, char** arglist){
-    int pipe_fd[2];
-    arglist[pipe_index] = NULL;
-
-    pid_t cpid;
-    if (pipe(pipe_fd) == -1){
-        printf("failed creating pipe!\n");
-        exit(1);
-    }
-
-
-    printf("i am here!\n");
-    cpid = fork();
-    //printf("cpid is %d!\n", cpid);
-    // child
-    if (cpid == 0){
-        // printf("Child - command is:\n");
-        // TODO - can it fail?
-        dup2(pipe_fd[0], STDIN_FILENO);
-        close(pipe_fd[0]);
-        close(pipe_fd[1]);
-
-        int index = pipe_index + 1;
-        while (arglist[index] != NULL){
-            //printf("%s \n", arglist[index]);
-            index ++;
-        }
-
-        if (execvp(arglist[pipe_index+1], &(arglist[pipe_index + 1])) == -1){
-            // TODO - change it!
-            printf("yeled efes!\n");
-            exit(1);
-        }
-    }
-
-    // father
-    else{
-        printf("Father - command is:\n");
-        // TODO - can it fail?
-        dup2(pipe_fd[1], STDOUT_FILENO);
-        close(pipe_fd[0]);
-        close(pipe_fd[1]);
-
-        sleep(5);
-        if (execvp(arglist, arglist) == -1){
-            // TODO - change it!
-            printf("aba marbitz!\n");
-            exit(1);
-        }
-    }
+static void signal_ignore_handler(int signal, siginfo_t *info, void *context){
+    printf("bla bla\n");
 }
 
 int process_arglist(int count, char** arglist){
+
+    struct sigaction ignore_signal;
+    memset(&ignore_signal, 0, sizeof(ignore_signal));
+    ignore_signal.sa_handler = SIG_IGN;
+    struct sigaction default_signal;
+    memset(&default_signal, 0, sizeof(default_signal));
+    default_signal.sa_handler = SIG_DFL;
+
+    //ignore_signal.sa_sigaction = signal_ignore_handler;
+    if (sigaction(SIGINT, &ignore_signal, NULL) != 0){
+        printf("error initializing signal");
+        return 1;
+    }
 
     bool run_in_background = false;
     int pipe_index = -1;
@@ -104,6 +79,16 @@ int process_arglist(int count, char** arglist){
     pipe_index = locate_pipe(count, arglist);
     printf("%d\n", pipe_index);
 
+
+
+    int pipe_fd[2] = {0};
+    if (pipe_index != -1){
+        if (pipe(pipe_fd)){
+            printf("error in pipe!");
+            return 1;
+        }
+    }
+
     pid_t pid = fork();
 
     // we are in child process
@@ -111,19 +96,78 @@ int process_arglist(int count, char** arglist){
         if (pipe_index == -1) {
             // ignore the last argument
             if (run_in_background == true) {
+                //sigaction(SIGINT, &ignore_signal, NULL);
+                printf("child in bg %d\n", getpid());
                 arglist[count - 1] = NULL;
                 take_care_of_single_child(arglist);
             }
                 // don't ignore the last argument
             else {
+                printf("child in fg %d\n", getpid());
+                sigaction(SIGINT, &default_signal, NULL);
                 take_care_of_single_child(arglist);
             }
-        } else {
-            take_care_of_two_children(pipe_index, arglist);
+        }
+
+        else{
+            sigaction(SIGINT, &default_signal, NULL);
+            close(pipe_fd[0]);
+            dup2(pipe_fd[1], 1);
+            close(pipe_fd[1]);
+            arglist[pipe_index] = NULL;
+            // TODO - wrap execvp
+            execvp(arglist[0], arglist);
         }
     }
-    int status;
-    while (wait(&status) != -1);
+
+
+    pid_t pid_2;
+
+    if (pipe_index != -1) {
+
+        pid_2 = fork();
+
+        if (pid_2 == 0) {
+
+            printf("*** %d\n", getpid());
+
+            sigaction(SIGINT, &default_signal, NULL);
+            close(pipe_fd[1]);
+            dup2(pipe_fd[0], 0);
+            close(pipe_fd[0]);
+            printf("%s\n", arglist[pipe_index+1]);
+            // arglist[pipe_index] = NULL;
+            execvp(arglist[pipe_index+1], &arglist[pipe_index+1]);
+        }
+
+        close(pipe_fd[0]);
+        close(pipe_fd[1]);
+    }
+
+    if (run_in_background){
+        printf("run in background\n");
+        return 1;
+    }
+
+    int status = 0;
+    int status2 = 0;
+    if (pipe_index == -1) {
+        if (wait(&status) == pid) {
+            printf("### pid: %d, pid2: %d\n", pid, pid_2);
+            printf("child finished\n");
+
+            // TODO - print child status?
+            return 1;
+        }
+    }
+
+    else{
+        if ((waitpid(pid, &status, 0) == pid) && (waitpid(pid_2, &status2, 0) == pid_2)){
+            printf("### pid: %d, pid2: %d\n", pid, pid_2);
+            // TODO - print child status?
+            return 1;
+        }
+    }
     return 1;
 }
 
@@ -139,7 +183,13 @@ int finalize(void){
 }
 
 int main(int argc,char** argv){
-    char* arglist[] = {"ls", "-la", "|", "grep", "-i", "hw", NULL};
-    process_arglist(6, arglist);
+    char* arglist0[] = {"ls", "-la", "|", "grep", "-i", "hw", NULL};
+    process_arglist(6, arglist0);
+
+    char* arglist2[] = {"sleep", "70", NULL};
+    process_arglist(2, arglist2);
+
+    char* arglist[] = {"sleep", "60", "&", NULL};
+    process_arglist(3, arglist);
 }
 
