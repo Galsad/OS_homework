da0a8501f9c53ba655e8ecbdc5a94ae7e770f490
Galsad <galsdh@gmail.com>
2017-11-29 10:48:50 -0800

    zombies are killed!

--- myshell.c
+++ myshell.c
@@ -7,18 +7,6 @@
 #include <signal.h>
 
 
-// TODO - errors should be written to strerr, need to write exit(1)
-// TODO - CTRL-D exits?
-// TODO - free struct memory (finalize)
-// TODO - move sig_action to prepare
-// TODO - make sure 1 is always returned from process arglist
-// TODO - check success of execvp, dup (and to execute exit(1)if needed)
-// TODO - take care of zombies (SIGCHILD)
-// TODO - prepare returns 0 on success
-// TODO - skeleton calls finilize and returns 0 on success
-// TODO - separate the waits in the while loops
-
-
 // locates the pipe index if exists
 int locate_pipe(int count, char ** arglist){
     for (int i=0; i<count; i++){
@@ -40,13 +28,9 @@ bool has_ampercent(int count, char** arglist){
 // run child command in case it's a legal one, if not, returns 0 - this function takes care only in case there is not |
 void take_care_of_single_child(char** arglist){
     if (execvp(arglist[0], arglist) == -1){
-        printf("child died!\n");
+        write(stderr,"execvp failed\n\0",15);
         exit(1);
     }
-}
-
-static void signal_ignore_handler(int signal, siginfo_t *info, void *context){
-    printf("bla bla\n");
 }
 
 int process_arglist(int count, char** arglist){
@@ -60,15 +44,15 @@ int process_arglist(int count, char** arglist){
 
     //ignore_signal.sa_sigaction = signal_ignore_handler;
     if (sigaction(SIGINT, &ignore_signal, NULL) != 0){
-        printf("error initializing signal");
-        return 1;
+        write(stderr,"error initializing signal\n\0",27);
+        exit(1);
     }
 
     bool run_in_background = false;
     int pipe_index = -1;
     // checks if there is positive amount of commands, if not, exit
     if (count <= 0){
-        printf("non positives amount of commands!\n");
+        write(stderr,"non positives amount of commands!\n",30);
         return 1;
     }
 
@@ -84,8 +68,8 @@ int process_arglist(int count, char** arglist){
     int pipe_fd[2] = {0};
     if (pipe_index != -1){
         if (pipe(pipe_fd)){
-            printf("error in pipe!");
-            return 1;
+            write(stderr,"error in pipe!\n\0",16);
+            exit(1);
         }
     }
 
@@ -97,13 +81,11 @@ int process_arglist(int count, char** arglist){
             // ignore the last argument
             if (run_in_background == true) {
                 //sigaction(SIGINT, &ignore_signal, NULL);
-                printf("child in bg %d\n", getpid());
                 arglist[count - 1] = NULL;
                 take_care_of_single_child(arglist);
             }
                 // don't ignore the last argument
             else {
-                printf("child in fg %d\n", getpid());
                 sigaction(SIGINT, &default_signal, NULL);
                 take_care_of_single_child(arglist);
             }
@@ -112,11 +94,13 @@ int process_arglist(int count, char** arglist){
         else{
             sigaction(SIGINT, &default_signal, NULL);
             close(pipe_fd[0]);
-            dup2(pipe_fd[1], 1);
+            if (dup2(pipe_fd[1], 1) == -1){
+                write(stderr,"error in dup!\n\0",15);
+                exit(1);
+            }
             close(pipe_fd[1]);
             arglist[pipe_index] = NULL;
-            // TODO - wrap execvp
-            execvp(arglist[0], arglist);
+            take_care_of_single_child(arglist);
         }
     }
 
@@ -128,16 +112,14 @@ int process_arglist(int count, char** arglist){
         pid_2 = fork();
 
         if (pid_2 == 0) {
-
-            printf("*** %d\n", getpid());
-
             sigaction(SIGINT, &default_signal, NULL);
             close(pipe_fd[1]);
-            dup2(pipe_fd[0], 0);
+            if (dup2(pipe_fd[0], 0) == -1){
+                write(stderr,"error in dup!\n\0",15);
+                exit(1);
+            }
             close(pipe_fd[0]);
-            printf("%s\n", arglist[pipe_index+1]);
-            // arglist[pipe_index] = NULL;
-            execvp(arglist[pipe_index+1], &arglist[pipe_index+1]);
+            take_care_of_single_child(&(arglist[pipe_index + 1]));
         }
 
         close(pipe_fd[0]);
@@ -145,26 +127,19 @@ int process_arglist(int count, char** arglist){
     }
 
     if (run_in_background){
-        printf("run in background\n");
         return 1;
     }
 
     int status = 0;
     int status2 = 0;
     if (pipe_index == -1) {
-        if (wait(&status) == pid) {
-            printf("### pid: %d, pid2: %d\n", pid, pid_2);
-            printf("child finished\n");
-
-            // TODO - print child status?
+        if (waitpid(pid, &status, 0) == pid) {
             return 1;
         }
     }
 
     else{
         if ((waitpid(pid, &status, 0) == pid) && (waitpid(pid_2, &status2, 0) == pid_2)){
-            printf("### pid: %d, pid2: %d\n", pid, pid_2);
-            // TODO - print child status?
             return 1;
         }
     }
@@ -174,22 +149,30 @@ int process_arglist(int count, char** arglist){
 // prepare and finalize calls for initialization and destruction of anything required
 
 int prepare(void){
-
+    struct sigaction child_signal;
+    memset(&child_signal, 0, sizeof(child_signal));
+    child_signal.sa_handler = SIG_IGN;
+    sigaction(SIGCHLD, &child_signal, NULL);
+    return 0;
 }
 
 
 int finalize(void){
-
+    return 0;
 }
 
 int main(int argc,char** argv){
+    prepare();
+
     char* arglist0[] = {"ls", "-la", "|", "grep", "-i", "hw", NULL};
     process_arglist(6, arglist0);
 
-    char* arglist2[] = {"sleep", "70", NULL};
-    process_arglist(2, arglist2);
+    char* arglist2[] = {"sleep", "10", "&", NULL};
+    process_arglist(3, arglist2);
 
-    char* arglist[] = {"sleep", "60", "&", NULL};
-    process_arglist(3, arglist);
+    char* arglist[] = {"sleep", "60", NULL};
+    process_arglist(2, arglist);
+
+    finalize();
 }
 
